835845691a96af8582429933981d965fa55bdbe8
uliasuprun <uliasuprun88@gmail.com>
2022-07-29 17:33:45 +0300

    task-02 is done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const createLi = ingredients.map(ingredient => {
+  const newLi = document.createElement("li");
+  newLi.textContent = ingredient;
+  newLi.classList.add("item");
+  return newLi;
+})
+
+ingredientsEl.append(...createLi);
+
+console.log(createLi);
