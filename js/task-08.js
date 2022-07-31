e0b866e1e5e32195a60885d1a518a0b9657ce79d
uliasuprun <uliasuprun88@gmail.com>
2022-07-31 18:10:15 +0300

    task 08 is done

--- js/task-08.js
+++ js/task-08.js
@@ -5,17 +5,14 @@ form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
 
-
-
-    console.log('this is form submit');
-
-
-
-
     const formElements = event.currentTarget.elements;
     const mail = formElements.email.value;
     const password = formElements.password.value;
-    // console.log('email:', mail, 'password:', password);
-    console.log(`email: ${mail} password: ${password}`);
+    console.log('email:', mail, 'password:', password);
 
+    if (mail && password !== '') {
+        return;
+    } else {
+        alert('Все поля должны быть заполнены')
+    }
 }
