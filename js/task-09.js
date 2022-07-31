1a79f7e704b67eadf47994ff01a0f31f44ebd3d1
uliasuprun <uliasuprun88@gmail.com>
2022-07-31 17:46:27 +0300

    task 09 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const spanColorBtn = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeBodyColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBodyColor() {
+  const color = getRandomHexColor();
+  body.style.background = color;
+  spanColorBtn.textContent = color;
+  }
